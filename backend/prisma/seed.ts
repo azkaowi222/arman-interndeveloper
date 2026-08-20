665771507af28e86aa67dec7fc7112f1556da293
Arman Maulana <azkaowi@gmail.com>
2026-08-20 22:20:47 +0700

    edit seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -12,40 +12,39 @@ async function main() {
         role: "JOB_SEEKER",
       },
     });
-    console.log("ok");
-    // const hashedPasswordJobSeeker = await bcrypt.hash("jobseeker", 10);
-    // await prisma.user.create({
-    //   data: {
-    //     name: "jobseeker",
-    //     email: "jobseeker@gmail.com",
-    //     password: hashedPasswordJobSeeker,
-    //     role: "JOB_SEEKER",
-    //   },
-    // });
+    const hashedPasswordJobSeeker = await bcrypt.hash("jobseeker", 10);
+    await prisma.user.create({
+      data: {
+        name: "jobseeker",
+        email: "jobseeker@gmail.com",
+        password: hashedPasswordJobSeeker,
+        role: "JOB_SEEKER",
+      },
+    });
 
-    // console.log("user jobseeker berhasil dibuat");
+    console.log("user jobseeker berhasil dibuat");
 
-    // const hashedPasswordCompany = await bcrypt.hash("company", 10);
-    // const companieUser = await prisma.user.create({
-    //   data: {
-    //     name: "company",
-    //     email: "company@gmail.com",
-    //     password: hashedPasswordCompany,
-    //     role: "COMPANY",
-    //   },
-    // });
-    // console.log("user company berhasil dibuat");
+    const hashedPasswordCompany = await bcrypt.hash("company", 10);
+    const companieUser = await prisma.user.create({
+      data: {
+        name: "company",
+        email: "company@gmail.com",
+        password: hashedPasswordCompany,
+        role: "COMPANY",
+      },
+    });
+    console.log("user company berhasil dibuat");
 
-    // const companie = await prisma.company.create({
-    //   data: {
-    //     companyName: "PT. INDONESIA MAJU",
-    //     description: "Memakmurkan dan mensejahterakan bangsa indonesia",
-    //     location: "Tangerang, Banten",
-    //     userId: companieUser.id,
-    //   },
-    // });
+    const companie = await prisma.company.create({
+      data: {
+        companyName: "PT. INDONESIA MAJU",
+        description: "Memakmurkan dan mensejahterakan bangsa indonesia",
+        location: "Tangerang, Banten",
+        userId: companieUser.id,
+      },
+    });
 
-    // console.log(`companie ${companie.companyName} berhasil dibuat`);
+    console.log(`companie ${companie.companyName} berhasil dibuat`);
   } catch (error) {
     console.log({
       error:
