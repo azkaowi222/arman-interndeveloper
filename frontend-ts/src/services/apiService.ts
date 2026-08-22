c4cf9b6f75c182c2a8aafc9fa335d42d71b66a49
Arman Maulana <azkaowi@gmail.com>
2026-08-22 16:39:58 +0700

    edit endpoint api

--- frontend-ts/src/services/apiService.ts
+++ frontend-ts/src/services/apiService.ts
@@ -1,5 +1,5 @@
-const backendUrl = "http://localhost:3000/api";
-// const backendUrl =
-//   "https://backend-indokerja-new-a9eshmecepdbc5ej.indonesiacentral-01.azurewebsites.net/api";
+// const backendUrl = "http://localhost:3000/api";
+const backendUrl =
+  "https://backend-indokerja-new-a9eshmecepdbc5ej.indonesiacentral-01.azurewebsites.net/api";
 
 export default backendUrl;
